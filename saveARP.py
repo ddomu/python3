af64e8a4ac9dee6d0842cb4c6c70e2a5b9e3cf76
ddomu <dalbi.lee@gmail.com>
2018-12-14 14:07:34 -0500

    Update saveARP.py

--- saveARP.py
+++ saveARP.py
@@ -2,45 +2,58 @@ import json
 import re
 
 fiel = "i.json"
-
+ip = "10.1.2.1"
+#filename is ip address
 # f = open(fiel, 'r')
 
-l = json.loads(i)
+l = json.loads(ip)
 
+va={}
+ipmaif = [] # ip,mac,if
 # IP Search in ARP table # 
 for k, v in l.iteritems():
  print (k)
  for line in v[0:5]:
-  # print (line)
-  if "Interface" in line : # header line 
+  print (line)
+  if "Interface" in line : # header line  
     headers = re.split("\s\s+", line)
-    print (headers) 
-  elif bool(ip_pattern.search(line)) : # MAC address exists  
-    print (line)
+    ip_index = headers.index("Address") 
+    mac_hw = "Hardware Addr" if "Hardware" in line else "MAC Address"  # MAC Address or Hardware Addr 
+    mac_index = headers.index(mac_hw) 
+    if_index = headers.index("Interface") 
+  elif bool(ip_pattern.search(line)) : # IP address exists
+# elif bool(mac_pattern.search(line)) : # MAC address exists  
+    contents = re.split("\s\s+", line)
+    if ip_index and mac_index and if_index : 
+     ipmaif.append([ip,k ] + map(contents.__getitem__, [ ip_index, mac_index, if_index] ))
+     #print (contents[if_index])
   else:
     #print ("no mac", line)
     print ("--------------")
-      
-  
-#  mac_pattern = re.compile(r'([0-9A-F]{4})' + '\.([0-9A-F]{4})'*2, re.IGNORECASE)
-mac_pattern = re.compile(r'([0-9A-F]{4})' + '\.([0-9A-F]{4})'*2, re.IGNORECASE)
-
-mac_pattern = re.compile(r'([0-9A-F]{4})' + '[\.:-]([0-9A-F]{4})'*2, re.IGNORECASE)
+# va[k] = ipmaif      
+for i in ipmaif[0:30]:
+  print (i)
+ 
+ 
+# mac_pattern = re.compile(r'([0-9A-F]{4})' + '\.([0-9A-F]{4})'*2, re.IGNORECASE)
+# mac_pattern = re.compile(r'([0-9A-F]{4})' + '\.([0-9A-F]{4})'*2, re.IGNORECASE)
+mac_pattern = re.compile(r'([0-9A-F]{4})' + '[\.:-]([0-9A-F]{4})'*2, re.IGNORECASE) # Update deliminator 
 
 ip_reg_exp = '[1-9](\d{0,2})(\.\d{1,3}){3}'
 ip_pattern = re.compile(ip_reg_exp)
 
-# MAC  Search in ARP table # 
-for k, v in l.iteritems():
- print (k)
- for line in v[0:5]:
-  # print (line)
-  if "Interface" in line : # header line 
-    headers = re.split("\s\s+", line)
-    print (headers) 
-  elif bool(mac_pattern.search(line)) : # MAC address exists  
-    print (line)
-  else:
-    print ("no mac", line)
-    #print ("--------------")
-    
+# NX OS Header 
+#"Address         Age       MAC Address     Interface"
+
+# IOS Header
+#"Protocol  Address          Age (min)  Hardware Addr   Type   Interface"
+
+
+#### Get file list in directory 
+from os import listdir
+from os.path import isfile, join
+
+
+mypath = "./arp"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+ip=onlyfiles[-1]
